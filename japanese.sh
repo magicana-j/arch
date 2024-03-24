09c5027f3d38d0bfeee3828ae1966aa8b552ab56
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-03-24 18:31:16 +0900

    Create japanese.sh

---
+++ japanese.sh
@@ -0,0 +1,9 @@
+sudo pacman -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
+sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
+sudo cat <<EOL >> /etc/environment
+
+XMODIFIERS=@im=fcitx
+GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+EOL
+exec fcitx5
