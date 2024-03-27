2b8ed4c6dbbf43aac4a96717a04c2238e79f30bb
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-03-27 19:15:34 +0900

    Add files via upload

--- virtmanager.sh
+++ virtmanager.sh
@@ -2,6 +2,6 @@ sudo pacman -Syu qemu virt-manager
 sudo systemctl enable --now libvirtd
 sudo usermod -aG libvirt "$USER"
 yay -S dnsmasq
-virsh net-define /etc/libvirt/qemu/networks/default.xml
+sudo virsh net-define /etc/libvirt/qemu/networks/default.xml
 sudo virsh net-start default
 sudo virsh net-autostart default
