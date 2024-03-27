4529da7bed91c858a6a2e213a8b66f7fde96ab17
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-03-27 17:19:03 +0900

    Add files via upload

--- fcitx5.sh
+++ fcitx5.sh
@@ -1,9 +1,5 @@
-sudo pacman -Sy
-sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
-sudo cat <<EOL >> /etc/environment
-
-XMODIFIERS=@im=fcitx
-GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
-EOL
+sudo pacman -Syu fcitx5-im fcitx5-mozc fcitx5-configtool
+echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
+echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
 exec fcitx5 -d
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sh ./firstinstall.sh
-sh ./yay.sh
 sh ./fcitx5.sh
 sh ./steam.sh
+sh ./yay.sh
