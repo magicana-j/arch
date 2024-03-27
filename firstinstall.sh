69f9b7713543127f62a03f25601b9605372015d6
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-03-27 11:35:14 +0900

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -1,4 +1,7 @@
 sudo -Syyu
-sudo pacman -S vim nano less timeshift firefox ufw gufw xdg-user-dirs-gtk gnome-keyring
+sudo pacman -S vim nano less timeshift neofetch wget curl
+sudo pacman -S ufw gufw xdg-user-dirs-gtk gnome-keyring
+sudo pacman -S firefox
+sudo pacman -S go
 sudo pacman -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
 LANG=C xdg-user-dirs-gtk-update
