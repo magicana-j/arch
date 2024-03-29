a59aba24bcfc2a9e0b7583834d36f2380a99c6b4
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-03-29 22:38:54 +0900

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -5,7 +5,7 @@ sudo pacman -S firefox
 sudo pacman -S go
 sudo pacman -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
 LANG=C xdg-user-dirs-gtk-update
-sudo sed -i.bak -r ‘s/^#(ja_JP.UTF-8)/\1/’ /etc/locale.gen
+sudo sed -i.bak -r ‘s/^#(ja_JP.UTF-8)/\1/i’ /etc/locale.gen
 sudo localectl set-keymap jp106
 sudo locale-gen
 sudo localectl set-locale ja_JP.utf-8
