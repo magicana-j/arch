70b4f0a95eff329bd27e2e2951a5d2c4dca409a6
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-12 06:53:11 +0900

    Create japanese.sh

---
+++ japanese.sh
@@ -0,0 +1,7 @@
+sudo pacman -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
+LANG=C xdg-user-dirs-gtk-update
+sudo sed -i.bak -r ‘s/^#(ja_JP.UTF-8)/\1/i’ /etc/locale.gen
+sudo localectl set-keymap jp106
+sudo locale-gen
+sudo localectl set-locale ja_JP.utf-8
+# echo “export LANG=C” | sudo tee -a ~/.bashrc
