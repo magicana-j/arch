0c561650d6b9403dc0c8bc20c38ce1043782dc29
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-12 06:55:55 +0900

    Create hyprland.sh

---
+++ hyprland.sh
@@ -0,0 +1,7 @@
+sudo pacman -S ttf-font-awesome
+sudo pacman -S waybar alacritty thunar wofi dunst qt5-wayland qt6-wayland
+yay -S clipman
+
+sudo pacman -S --needed hyprland hyprpaper xdg-desktop-portal-hyprland
+
+yay -S pfetch
