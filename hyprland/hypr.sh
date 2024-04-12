c4d54273c556ac43f8542fab8790c242be51b0f4
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-13 06:58:37 +0900

    Update hypr.sh

--- hyprland/hypr.sh
+++ hyprland/hypr.sh
@@ -1,4 +1,4 @@
-yay -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot
+yay -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot wlogout
 yay -S --needed ttf-font-awesome #フォント
 
 # yay -S greetd-tuigreet # ログイン画面
@@ -7,10 +7,10 @@ yay -S --needed thunar dolphin thunar gvfs tumbler ffmpegthumbnailer # ファイ
 yay -S --needed alacritty
 yay -S --needed gnome-keyring # キーリング
 yay -S --needed htop powertop smartmontools ufw # ユーティリティ
-yay -S hyprlock hypridle # ロック画面 自動ロック
+yay -S --needed hyprlock hypridle # ロック画面 自動ロック
 
-yay -S iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct # GUI関係
+yay -S iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct network-manager-applet # GUI関係
 # yay -S pipewire pipewire-pulse
-yay -S --needed mako dunst xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
+yay -S --needed mako xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
 
 yay -S --needed hyprland
