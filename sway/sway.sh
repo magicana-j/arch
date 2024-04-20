efa7a6920b6795e7ecdefb31d3666e081a157047
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-20 18:57:55 +0900

    sway.sh を更新

--- sway/sway.sh
+++ sway/sway.sh
@@ -1,4 +1,4 @@
 pacman -Sy grim slurp mako brightnessctl wf-recorder wob gammastep wl-clipboard clipman
 pacman -S ttf-font-awesome
-pacman -S dolphin waybar wofi
+pacman -S --needed thunar waybar wofi
 pacman -S sway swaybg xorg-xwayland qt5-wayland polkit dmenu foot
