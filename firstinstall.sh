b4b1a835b3398e6f5975757d704c50a447ff5595
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-20 18:45:17 +0900

    firstinstall.sh を更新

--- firstinstall.sh
+++ firstinstall.sh
@@ -1,5 +1,5 @@
 sudo -Syyu
-sudo pacman -S --needed vim nano less timeshift neofetch wget curl unzip
+sudo pacman -S --needed neovim vim nano less timeshift neofetch wget curl unzip
 sudo pacman -S ufw gufw xdg-user-dirs-gtk gnome-keyring
-sudo pacman -S firefox
+sudo pacman -S --needed firefox
 sudo pacman -S go
