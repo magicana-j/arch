ed134fde80c5f814cc50337c0025dfb1a2a9b997
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-29 16:33:31 +0900

    fcitx5.sh を更新

--- fcitx5.sh
+++ fcitx5.sh
@@ -1,27 +1,27 @@
 sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
 
-echo <<EOF >> ~/.bashrc
+cat << EOF >> ~/.bashrc
 export XMODIFIERS=@im=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 
 EOF
 
-echo <<EOF >> ~/.xprofile
+cat << EOF >> ~/.xprofile
 export XMODIFIERS=@im=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 
 EOF
 
-echo <<EOF >> ~/.xsession
+cat << EOF >> ~/.xsession
 export XMODIFIERS=@im=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 
 EOF
 
-echo <<EOF >> ~/.config/environment.d/im.conf
+echo << EOF >> ~/.config/environment.d/im.conf
 XMODIFIERS=@im=fcitx
 GTK_IM_MODULE=fcitx
 QT_IM_MODULE=fcitx
