16076789c4fa23e8c70c61d2a65fb03356bc9251
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-07-06 22:32:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 export LANG=C
-sh ./firstinstall.sh
-sh ./fcitx5.sh
-sh ./steam.sh
-sh ./yay.sh
+sudo -Syyu
+sudo pacman -S bluez blueman
+sudo pacman -S podman podman-compose
+sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
+
+#sh ./firstinstall.sh
+#sh ./fcitx5.sh
+#sh ./steam.sh
+#sh ./yay.sh
