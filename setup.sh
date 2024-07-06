a6a4942491118f83e0ac31ae98d32948cd6a6f6a
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-07-06 22:46:16 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 export LANG=C
 sudo -Syyu
-sudo pacman -S bluez blueman
-sudo pacman -S podman podman-compose
-sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
-
+sudo pacman -S --needed bluez blueman
+sudo pacman -S --needed podman podman-compose
+sudo pacman -S --needed fcitx5-im fcitx5-mozc fcitx5-configtool
+sudo pacman -S --needed pacman-contrib neovim vim nano less timeshift neofetch wget curl unzip
+sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
+sudo pacman -S --needed firefox
+sudo pacman -S --needed go
 #sh ./firstinstall.sh
 #sh ./fcitx5.sh
 #sh ./steam.sh
