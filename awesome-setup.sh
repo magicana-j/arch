d2f148c1fe335f91aa6c5a4ad57791d9c8785682
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-30 19:58:07 +0900

    Create awesome-setup.sh

---
+++ awesome-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo pacman -Syu
+sudo pacman -S xorg-fonts ttf-liberation xorg-xrandr slock xorg-server xsel
+sudo pacman -S alacritty feh xterm terminus-font xorg-xinit
+sudo pacman -S awesome
