98c95884c4a9ce081900845ef944aab7d2d60485
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-03 15:39:15 +0900

    Update hypr-setup.sh

--- hyprland/hypr-setup.sh
+++ hyprland/hypr-setup.sh
@@ -1,17 +1,16 @@
-yay -S --needed fcitx5{-im,mozc,configtool}
-yay -S --needed adobe-source{-code-pro-fonts,-sans-fonts,-serif-fonts,-han-sans-jp-fonts,-han-serif-jp-fonts}
-yay -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot wlogout
-yay -S --needed ttf-font-awesome #フォント
+#yay -S --needed fcitx5{-im,mozc,configtool}
+#yay -S --needed adobe-source{-code-pro-fonts,-sans-fonts,-serif-fonts,-han-sans-jp-fonts,-han-serif-jp-fonts}
+#yay -S --needed iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct # GUI関係
+#yay -S greetd-tuigreet # ログイン画面
+#yay -S --needed gnome-keyring # キーリング
+#yay -S --needed htop powertop smartmontools ufw # ユーティリティ
 
-# yay -S greetd-tuigreet # ログイン画面
-yay -S --needed dolphin thunar gvfs tumbler ffmpegthumbnailer # ファイルマネージャ
-yay -S --needed kitty foot alacritty xfce4-terminal wezterm
-yay -S --needed gnome-keyring # キーリング
-yay -S --needed htop powertop smartmontools ufw # ユーティリティ
-yay -S --needed hyprlock hypridle # ロック画面 自動ロック
+sudo pacman -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot wlogout network-manager-applet pavucontrol mako
+sudo pacman -S --needed ttf-font-awesome #フォント
 
-yay -S --needed iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct network-manager-applet # GUI関係
-# yay -S pipewire pipewire-pulse
-yay -S --needed mako xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
-
-yay -S --needed hyprland
+sudo pacman -S --needed dolphin thunar gvfs tumbler ffmpegthumbnailer # ファイルマネージャ
+sudo pacman -S --needed kitty foot alacritty xfce4-terminal wezterm
+sudo pacman -S --needed hyprlock hypridle # ロック画面 自動ロック
+sudo pacman -S --needed xdg-desktop-portal-hyprland qt5-wayland qt6-wayland
+sudo pacman -S hyprland
+#yay -S --needed hyprland-git
