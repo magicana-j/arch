1c3865d9867f6be99d08f48bd98b54a0b13026fc
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-03 15:13:04 +0900

    Update i3-setup.sh

--- i3-setup.sh
+++ i3-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -S dmenu xterm rofi lightdm lightdm-gtk-greeter
-sudo pacman -S i3-wm i3blocks i3status i3lock
+sudo pacman -S --needed dmenu xterm rofi lightdm lightdm-gtk-greeter
+sudo pacman -S --needed i3-wm i3blocks i3status i3lock polybar volumeicon network-manager-applet
