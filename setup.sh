52af5af0e83e1a5f70e06b2b439fb293a4bbacf9
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-16 17:56:39 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 export LANG=C
 sudo -Syyu
-sudo pacman -S --needed bluez blueman
-sudo pacman -S --needed podman podman-compose
-sudo pacman -S --needed fcitx5-im fcitx5-mozc fcitx5-configtool
-sudo pacman -S --needed pacman-contrib neovim vim nano less timeshift neofetch wget curl unzip
-sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
 sudo pacman -S --needed firefox
 sudo pacman -S --needed go
-#sh ./firstinstall.sh
-#sh ./fcitx5.sh
-#sh ./steam.sh
-#sh ./yay.sh
+sudo pacman -S --needed noto-fonts-{cjk, extra, configtool}
+sudo pacman -S --needed bluez blueman
+sudo pacman -S --needed flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo pacman -S --needed network-manager-applet
+sudo pacman -S --needed podman
+sudo pacman -S --needed fcitx5-im fcitx5-mozc fcitx5-configtool
+sudo pacman -S --needed pacman-contrib neovim vim nano less timeshift fastfetch neofetch wget curl unzip zip p7zip
+sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
