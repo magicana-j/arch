62781b1aba6ab463b1e4c9ab0bca7707ef3ca68c
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-22 16:04:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo pacman -S --needed network-manager-applet
 sudo pacman -S --needed podman
 sudo pacman -S --needed fcitx5-im fcitx5-mozc fcitx5-configtool
-sudo pacman -S --needed pacman-contrib neovim vim nano less timeshift fastfetch neofetch wget curl unzip zip p7zip bash-completion
+sudo pacman -S --needed pacman-contrib neovim vim nano less timeshift fastfetch neofetch wget curl unzip zip p7zip xarchiver bash-completion
 sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
