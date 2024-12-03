1244284750ad0752d74825e12055bda17a5c8649
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-03 17:25:22 +0900

    Update sway-setup.sh

--- sway-setup.sh
+++ sway-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -S --needed brightnessctl rofi wofi grim pavucontrol slurp xorg-xwayland
+sudo pacman -S --needed brightnessctl rofi wofi grim pavucontrol slurp xorg-xwayland xorg-server-xwayland
 sudo pacman -S --needed kitty foot alacritty xfce4-terminal wezterm
-sudo pacman -S --needed sway swaybg swayidle swaylock waybar mako volumeicon network-manager-applet
+sudo pacman -S --needed sway swaybg swayidle swaylock waybar mako volumeicon network-manager-applet wl-clipboard clipman
