eea12bceebd8558ead538b9fe67991e5a0b25af8
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-04 07:52:33 +0900

    Update sway-setup.sh

--- sway-setup.sh
+++ sway-setup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -S --needed brightnessctl rofi wofi grim pavucontrol slurp xorg-xwayland xorg-server-xwayland
-sudo pacman -S --needed kitty foot alacritty xfce4-terminal wezterm
-sudo pacman -S --needed sway swaybg swayidle swaylock waybar mako volumeicon network-manager-applet wl-clipboard clipman
+sudo pacman -S --needed wayland xorg-xwayland xorg-server-xwayland polkit \
+  wlr-arandr kanshi way-displays \
+  fuzzel foot alacritty nwg-drawer \
+  waybar swaybg swaylock waylock swayidle wayshot \
+  wf-recorder wl-clipboard clipman grimshot wl-screenrec wob obs-studio xwaylandvideobridge \
+  noro-fonts-cjk fcitx5-{im,configtool,mozc} \
+  brightnessctl pipewire pipewire-pulse pavucontrol wireplumber network-manager-applet \
+  firefox \
+  gvfs ntfs3g xdg-user-dirs-gtk bluez blueman \
+  xarchiver nemo mako volumeicon \
+  sway
