16ccfa0bfba3b1e95bf3e8885981697030fbb6ec
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-04 08:01:13 +0900

    Add files via upload

--- sway-setup.sh
+++ sway-setup.sh
@@ -2,13 +2,12 @@
 
 sudo pacman -Syu
 sudo pacman -S --needed wayland xorg-xwayland xorg-server-xwayland polkit \
-  wlr-arandr kanshi way-displays \
   fuzzel foot alacritty nwg-drawer \
-  waybar swaybg swaylock waylock swayidle wayshot \
-  wf-recorder wl-clipboard clipman grimshot wl-screenrec wob obs-studio xwaylandvideobridge \
-  noro-fonts-cjk fcitx5-{im,configtool,mozc} \
+  waybar swaybg swaylock waylock swayidle \
+  wf-recorder wl-clipboard wob obs-studio xwaylandvideobridge \
+  noto-fonts-cjk fcitx5-{im,configtool,mozc} \
   brightnessctl pipewire pipewire-pulse pavucontrol wireplumber network-manager-applet \
   firefox \
   gvfs ntfs-3g xdg-user-dirs-gtk bluez blueman \
-  xarchiver nemo nemo-fileroller mako volumeicon \
+  xarchiver nemo nemo-fileroller mako volumeicon kanshi \
   sway
