f638685294107ffa3c59eb59aca09dc730d2c685
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-03 17:22:53 +0900

    Update and rename setup.sh to basic-setup.sh

--- basic-setup.sh
+++ basic-setup.sh
@@ -7,13 +7,13 @@ sudo pacman -S --needed bluez blueman
 sudo pacman -S --needed flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo pacman -S --needed network-manager-applet
-sudo pacman -S --needed podman
+sudo pacman -S --needed podman podman-compose
 sudo pacman -S --needed fcitx5-im fcitx5-mozc fcitx5-configtool
-cat << EOF >> ~/.xprofile
+cat << EOF >> ~/.xinitrc
 export XMODIFIERS=@im=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 EOF
 sudo pacman -S --needed lib32-mesa lib32-vulkan-intel
-sudo pacman -S --needed gnome-software gparted pacman-contrib neovim vim nano less timeshift fastfetch neofetch wget curl unzip zip p7zip xarchiver bash-completion
+sudo pacman -S --needed gparted pacman-contrib neovim vim nano less timeshift htop gtop fastfetch neofetch wget curl unzip zip p7zip xarchiver bash-completion
 sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
