08a3fb6bc120601fcda3289362fadb0cdce0c608
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-04 08:16:22 +0900

    Delete sway directory

--- sway/sway.sh
+++
@@ -1,4 +0,0 @@
-pacman -Sy grim slurp mako brightnessctl wf-recorder wob gammastep wl-clipboard clipman
-pacman -S ttf-font-awesome
-pacman -S --needed thunar waybar wofi
-pacman -S sway swaybg xorg-xwayland qt5-wayland polkit dmenu foot
