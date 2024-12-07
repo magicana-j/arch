dd8ae48941dc61b5a85e3626227d39f15c9a1a89
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 18:11:22 +0900

    Update and rename tools.sh to office-setup.sh

---
+++ office-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo pacman -Syyu
+sudo pacman -S libreoffice-fresh(,-ja}
--- tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo pacman -Syyu
-sudo pacman -S zip unzip p7zip xarchiver
-sudo pacman -S shotwell gimp inkscape
-sudo pacman -S libreoffice-fresh(,-ja}
