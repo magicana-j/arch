1ae06db331c5d65854b61f1b7079c03fda2e0e38
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 18:26:49 +0900

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -1,2 +1,6 @@
-sudo pacman -S steam
+#!/bin/sh
+#sudo pacman -S --needed xf86-video-amdgpu xf86-video-ati mesa lib32-mesa vulkan-radeon vulkan-amdvlk lib32-vulkan-radeon lib32-amdvlk
+sudo pacman -S --needed xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel
+#sudo pacman -S --needed xf86-video-nouveau mesa lib32-mesa vulkan-nouveau lib32-vulkan-nouveau
 
+sudo pacman -S steam
