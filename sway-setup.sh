954ed979903aa87863d9278448e7dcc2b9e6c24d
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 14:31:48 +0900

    sway-setup.sh を更新

--- sway-setup.sh
+++ sway-setup.sh
@@ -5,11 +5,10 @@ sudo pacman -S --needed wayland xorg-xwayland xorg-server-xwayland polkit \
   fuzzel foot alacritty nwg-drawer \
   waybar swaybg swaylock waylock swayidle \
   wf-recorder wl-clipboard wob obs-studio xwaylandvideobridge \
-  noto-fonts-cjk fcitx5-{im,configtool,mozc} \
-  brightnessctl pipewire pipewire-pulse pavucontrol wireplumber network-manager-applet \
-  firefox \
+  brightnessctl pavucontrol network-manager-applet \
+  firefox nemo nemo-fileroller thunar thunar-archive-plugin \
   gvfs ntfs-3g xdg-user-dirs-gtk bluez blueman \
-  xarchiver nemo nemo-fileroller mako volumeicon kanshi \
+  xarchiver mako volumeicon kanshi \
   sway
 
 cp -rf ./config/* ~/.config/
