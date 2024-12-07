b0b6eea9bac68fe326321168983c5d3365b573e5
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 19:33:35 +0900

    Update and rename niri/niri-install.sh to niri-install.sh

---
+++ niri-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo pacman -Sy --needed wayland niri mako waybar swaybg swayidle alacritty fuzzel
+mkdir -p .config/niri
+
+#mkdir -p .config/waybar
+#cp /etc/xdg/waybar/* ~/.config/waybar/
+#mkdir -p .config/fuzzel
+#cp /etc/xdg/fuzzel/fuzzel.ini ~/.config/fuzzel/
