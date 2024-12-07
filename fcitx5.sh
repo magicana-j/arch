ae7f5556bad92ca73e24b368cbcea3a66c219976
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 14:27:38 +0900

    fcitx5.sh を更新

--- fcitx5.sh
+++ fcitx5.sh
@@ -1,24 +1,10 @@
 sudo pacman -S fcitx5-im fcitx5-mozc fcitx5-configtool
 
-cat << EOF >> ~/.bashrc
-export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-
-EOF
-
 cat << EOF >> ~/.xprofile
+setxkmap -layout jp
 export XMODIFIERS=@im=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
-
-EOF
-
-cat << EOF >> ~/.xsession
-export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-
 EOF
 
 cat << EOF > ~/.config/environment.d/im.conf
