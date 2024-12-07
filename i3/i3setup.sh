be96f06a95b0640acac6f8704fa28bfe5b466cba
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 19:35:21 +0900

    Delete i3 directory

--- i3/i3setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S i3-wm i3status i3blocks i3lock dunst dmenu alacritty grim slurp picom ttf-font-awesome
-
-mkdir -p ~/.config/i3blocks
-mkdir -p ~/.config/i3status
-cp /usr/local/etc/i3/i3blocks.conf.sample ~/.config/i3blocks/conf
-cp /usr/local/etc/i3/i3status.conf.sample ~/.config/i3status/conf
-
