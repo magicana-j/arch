ad98fba6dc6e0b7e935c473286b534bde9af3efa
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-08 20:16:58 +0900

    Update and rename hypr-setup.sh to hyprland-setup.sh

---
+++ hyprland/hyprland-setup.sh
@@ -0,0 +1,13 @@
+sudo pacman -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot wlogout network-manager-applet pavucontrol mako
+sudo pacman -S --needed ttf-font-awesome #フォント
+yay -S --needed htop powertop smartmontools ufw # ユーティリティ
+#yay -S --needed iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct # GUI関係
+#yay -S greetd-tuigreet # ログイン画面
+#yay -S --needed gnome-keyring # キーリング
+
+sudo pacman -S --needed dolphin thunar gvfs nemo tumbler ffmpegthumbnailer # ファイルマネージャ
+sudo pacman -S --needed kitty foot alacritty gnome-terminal
+sudo pacman -S --needed hyprlock hypridle # ロック画面 自動ロック
+sudo pacman -S --needed xdg-desktop-portal-hyprland xorg-xwayland xwaylandvideobridge qt5-wayland qt6-wayland
+sudo pacman -S hyprland
+#yay -S --needed hyprland-git
