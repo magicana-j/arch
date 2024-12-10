436a79119444facca2851a5f155f4c3bb425db4f
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-10 18:37:36 +0900

    Update sway-setup.sh

--- sway-setup.sh
+++ sway-setup.sh
@@ -2,14 +2,17 @@
 
 sudo pacman -Syu
 sudo pacman -S --needed wayland xorg-xwayland xorg-server-xwayland polkit \
-  kitty foot alacritty nwg-drawer \
-  waybar swaybg swaylock swayidle \
-  wf-recorder wl-clipboard wob obs-studio xwaylandvideobridge \
-  brightnessctl pavucontrol network-manager-applet \
-  firefox \
-  pcmanfm dolphin thunar thunar-archive-plugin \
-  gvfs ntfs-3g xdg-user-dirs-gtk bluez blueman \
+  xwaylandvideobridge qt5-wayland qt6-wayland
+
+sudo pacman -S --needed kitty foot alacritty 
+sudo pacman -S --needed gvfs ntfs-3g xdg-user-dirs-gtk bluez blueman \
   xarchiver mako volumeicon kanshi \
-  sway
+sudo pacman -S --needed  nwg-drawer \
+  waybar swaybg swaylock swayidle \
+  wf-recorder wl-clipboard wob
+  
+sudo pacman -S --needed  brightnessctl pavucontrol network-manager-applet \
+  firefox obs-studio pcmanfm dolphin thunar thunar-archive-plugin
+sudo pacman -S --needed sway
 
 cp -rf ./config/* ~/.config/
