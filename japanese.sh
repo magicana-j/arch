2737e5c2d1d886aa3ba63875620b0d379fcad1cf
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 17:31:03 +0900

    Update japanese.sh

--- japanese.sh
+++ japanese.sh
@@ -1,4 +1,12 @@
 sudo pacman -S --needed noto-fonts-{cjk,extra,emoji} adobe-source-code-pro-fonts
+sudo pacman -S --needed fcitx5-{im,configtool,mozc}
+
+echo << EOF >> ~/.xprofile
+export XMODIFIERS=@im=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+EOF
+
 # LANG=C xdg-user-dirs-gtk-update
 # sudo sed -i.bak -r ‘s/^#(ja_JP.UTF-8)/\1/i’ /etc/locale.gen
 # sudo localectl set-keymap jp106
