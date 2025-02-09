4b5f91a5f1ab7af92cee1dc1d8e272bfbd7757f4
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 17:19:51 +0900

    Update xfce4-setup.sh

--- xfce4-setup.sh
+++ xfce4-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -S lightdm lightdm-gtk-greeter
+#sudo pacman -S lightdm lightdm-gtk-greeter
 sudo pacman -S gvfs pavucontrol xarchiver
 sudo pacman -S xfce4 xfce4-goodies
