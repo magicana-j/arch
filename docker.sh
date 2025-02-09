4394047e23393949471d549c8c6ae1fc2eeccbda
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 17:23:06 +0900

    Update and rename docker.sh to podman.sh

--- docker.sh
+++
@@ -1,4 +0,0 @@
-sudo pacman -S podman podman-compose
-sudo usermod -aG docker "$USER"
-sudo systemctl start docker
-sudo systemctl enable docker
---
+++ podman.sh
@@ -0,0 +1,2 @@
+sudo pacman -S podman podman-compose
+echo 'Edit /etc/containers/registries.conf.'
