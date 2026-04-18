7aa2c1947fcb3ca3ff67fa89a8eb470e5e64fcaf
magicana-j <amuharai@example.dom>
2026-04-18 09:14:04 +0900

    Add flathub.sh

---
+++ flathub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+flatpak install flathub io.github.flattool.Warehouse
+flatpak install flathub sa.sy.bluerecorder
+flatpak install flathub org.tenacityaudio.Tenacity
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub org.ardour.Ardour
+flatpak install flathub org.videolan.VLC
+flatpak install flathub org.gnome.Boxes
+
